47b312e9bc77e4c56d243401e0e1e9f833214a39
Miguel <miguelcamargo9@gmail.com>
2020-09-13 16:10:43 -0500

    Adding the form of ITJ, models, services, views, component

---
+++ src/app/informeTecnicoJuridico/informeTecnicoJuridico.module.ts
@@ -0,0 +1,32 @@
+import { NgModule, LOCALE_ID } from "@angular/core";
+
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import { MaterialModule, UtilsModule } from "../app.module";
+import { CrearInformeTecnicoJuridicoComponent } from "./crear/CrearInformeTecnicoJuridico.component";
+import { InformeTecnicoJuridicoRoutes } from "./informeTecnicoJuridico.routing";
+import { InformeTecnicoJuridicoService } from "./informeTecnicoJuridico.service";
+import { DomainService } from "../domains/domain.service";
+import { MAT_DATE_LOCALE, DateAdapter, MAT_DATE_FORMATS } from "@angular/material";
+import { AppDateAdapter, APP_DATE_FORMATS } from "../core/format-datepicker/format-datepicker";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(InformeTecnicoJuridicoRoutes),
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    UtilsModule,
+  ],
+  declarations: [CrearInformeTecnicoJuridicoComponent],
+  providers: [
+    InformeTecnicoJuridicoService,
+    DomainService,
+    { provide: DateAdapter, useClass: AppDateAdapter },
+    { provide: MAT_DATE_FORMATS, useValue: APP_DATE_FORMATS },
+  ],
+})
+export class InformeTecnicoJuridicoModule {}
